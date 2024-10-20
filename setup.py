186a07727941957d5794e62121526c0c86266ab1
Yash Khatri <yashkhatri88540@gmail.com>
2024-10-20 10:52:01 +0530

    First setup commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)-> List[str]: 
+##this func will retun the lists of requiremennts
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace('\n', "") for req in requirements]
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Yash',
+    author_email='yashkhati88540@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
