ac3d3318d25526c419067a6d8c38d57e76329115
Yash Khatri <yashkhatri88540@gmail.com>
2024-10-27 22:27:14 +0530

    Data transformation

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from src.mlproject.logger import logging
-from src.mlproject.exxception import CustomException
+from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
+from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+
+
 import sys
+
 
 if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
-        #data_ingestion_config= DataIngestionConfig()
+        #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+       # data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+   
         
     except Exception as e:
-        logging.info("Custom exception")
+        logging.info("Custom Exception")
         raise CustomException(e,sys)
--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -1,45 +1,45 @@
 import os
 import sys
-from src.mlproject.exxception import CustomException
+from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 from src.mlproject.utils import read_sql_data
 from sklearn.model_selection import train_test_split
-
 from dataclasses import dataclass
-
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str=os.path.join('artifacts','train,csv')
-    test_data_path:str=os.path.join('artifacts','test,csv')
-    raw_data_path:str=os.path.join('artifacts','raw,csv')
-
-
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'raw.csv')
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionConfig()
+        self.ingestion_config = DataIngestionConfig()
+    
     def initiate_data_ingestion(self):
         try:
-            df=read_sql_data()
-            logging.info("Reading from mysql Database")
-            train_data_dir=os.path.dirname(self.ingestion_config.train_data_path )
-            os.makedirs(train_data_dir, exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            train_set,test_set=train_test_split(df,test_size=.2,random_state=42)
+            # Reading the data from csv
+            df = pd.read_csv(os.path.join('.dvc/notebook/data', 'raw.csv'))
+            logging.info("Reading completed from csv file")
             
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-            logging.info("Data ingesiton is complete")
-
-            return(
+            # Create directories if they don't exist
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            
+            # Save raw data
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            
+            # Split and save train/test sets
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            
+            logging.info("Data Ingestion is completed")
+            
+            return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
-        except Exception as e:
-            raise CustomException(e,sys)
         
-
-
+        except Exception as e:
+            raise CustomException(e, sys)
